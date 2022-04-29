1320dff1798c83f664dc4c112716fdad37453f73
nirbhay-design <sharma.59@iitj.ac.in>
2022-04-29 11:18:45 +0530

    adding files

--- dlops-project/config.py
+++ dlops-project/config.py
@@ -1,6 +1,6 @@
 import torch
 class configs(object):
-    epochs = 15
+    epochs = 100
     lr= 2e-4
     batch_size=16
     pin_memory = True
@@ -9,7 +9,7 @@ class configs(object):
     train_path = '/DATA/dataset/stldata/train_images'
     val_path = '/DATA/dataset/stldata/test_images'
     image_size= 256
-    gpu_id=7
+    gpu_id=6
     save_test_imgs = './test.svg'
     device= torch.device(f'cuda:{gpu_id}' if torch.cuda.is_available else 'cpu') 
     save_loss_path = './loss.svg'
